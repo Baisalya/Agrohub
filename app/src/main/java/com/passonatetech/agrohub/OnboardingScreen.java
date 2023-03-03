0e05ec5925aa4cef3265a77973b92a908b03121e
Baisalya <baishalya1999@gmail.com>
2023-03-03 11:48:11 +0530

    onbording overally fronted added

--- app/src/main/java/com/passonatetech/agrohub/OnboardingScreen.java
+++ app/src/main/java/com/passonatetech/agrohub/OnboardingScreen.java
@@ -1,14 +1,107 @@
 package com.passonatetech.agrohub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class OnboardingScreen extends AppCompatActivity {
+ViewPager AgroSliderViewPager;
+LinearLayout dotLayout;
+Button prevbtn,nextbtn,skipbtn;
+   TextView[] dots;
+    ViewerPageAdaptor viewerPageAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_screen);
+        prevbtn=findViewById(R.id.prev_btn);
+        nextbtn=findViewById(R.id.next_btn);
+        skipbtn=findViewById(R.id.skip_btn);
+       //Previous view in slider
+        prevbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              if(getItem(0)>0){
+                  AgroSliderViewPager.setCurrentItem(getItem(-1),true);
+              }
+            }
+        });
+        //next view in slider
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(getItem(0)<3){
+                    AgroSliderViewPager.setCurrentItem(getItem(1),true);
+                }else {
+                    Intent intent=new Intent(OnboardingScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        });
+        //skip onbording Screen
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(OnboardingScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //for viwpager
+        AgroSliderViewPager=(ViewPager) findViewById(R.id.SlideViewPager);
+        dotLayout=(LinearLayout) findViewById(R.id.indicator_layout);
+        viewerPageAdaptor = new ViewerPageAdaptor(this);
+        AgroSliderViewPager.setAdapter(viewerPageAdaptor);
+        setUpindicator(0);
+        AgroSliderViewPager.addOnPageChangeListener(viewListner);
+    }
+    //Method for adding indicator
+    public void  setUpindicator(int position){
+        dots=new TextView[4];
+        dotLayout.removeAllViews();
+
+        for (int i=0;i<dots.length;i++){
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.purple_200,getApplicationContext().getTheme()));
+            dotLayout.addView(dots[i]);
+        }
+        dots[position].setTextColor(getResources().getColor(R.color.purple_500,getApplicationContext().getTheme()));
+    }
+    ViewPager.OnPageChangeListener viewListner=new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+           setUpindicator(position);
+           if (position > 0){
+               prevbtn.setVisibility(View.VISIBLE);
+           }else {
+               prevbtn.setVisibility(View.INVISIBLE);
+           }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+    //method for give the position of viewItem
+    private int getItem(int i){
+        return AgroSliderViewPager.getCurrentItem()+i;
     }
 }
---
+++ app/src/main/java/com/passonatetech/agrohub/ViewerPageAdaptor.java
@@ -0,0 +1,70 @@
+package com.passonatetech.agrohub;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ViewerPageAdaptor extends PagerAdapter {
+    Context context;
+    int images[]={
+            R.drawable.leaf,
+            R.drawable.leaf,
+            R.drawable.leaf,
+            R.drawable.leaf,
+    };
+
+    int heading[]={
+            R.string.onboarding_text,
+            R.string.onboarding_text,
+            R.string.onboarding_text,
+            R.string.onboarding_text,
+    };
+    public ViewerPageAdaptor(Context context){
+        this.context=context;
+    }
+    int description[]={
+            R.string.app_name,
+            R.string.app_name,
+            R.string.app_name,
+            R.string.app_name,
+    };
+    @Override
+    public int getCount() {
+        return heading.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==(LinearLayout)object;
+    }
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container,int position){
+        LayoutInflater layoutInflater=(LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.slider_layout,container,false);
+        ImageView  slidetitleimage=(ImageView) view.findViewById(R.id.title_img);
+        TextView   slidingHeading=(TextView) view.findViewById(R.id.title_text);
+        TextView   slidingDescription=(TextView) view.findViewById(R.id.description_text);
+        //
+        slidetitleimage.setImageResource(images[position]);
+        slidingHeading.setText(heading[position]);
+        slidingDescription.setText(description[position]);
+        container.addView(view);
+        return view;
+
+       // return  super.instantiateItem(container,position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+       // super.destroyItem(container, position, object);
+        container.removeView((LinearLayout)object);
+    }
+}
