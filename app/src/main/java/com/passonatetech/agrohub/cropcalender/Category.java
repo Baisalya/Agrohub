0350dccdbc5b7c5658fe94d83e562c5f8420531f
Baisalya <baishalya1999@gmail.com>
2023-03-18 18:37:55 +0530

    few changes

--- app/src/main/java/com/passonatetech/agrohub/cropcalender/Category.java
+++ app/src/main/java/com/passonatetech/agrohub/cropcalender/Category.java
@@ -2,15 +2,26 @@ package com.passonatetech.agrohub.cropcalender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.passonatetech.agrohub.R;
 
 public class Category extends AppCompatActivity {
-
+   Button nxtbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        nxtbtn=findViewById(R.id.next_btn);
+        nxtbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Category.this,DetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
