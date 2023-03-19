d085779e6e0e369a3ab18b559db11c048e895393
Baisalya <baishalya1999@gmail.com>
2023-03-19 17:57:03 +0530

    login validtion

--- app/src/main/java/com/passonatetech/agrohub/loginsignup/LoginActivity.java
+++ app/src/main/java/com/passonatetech/agrohub/loginsignup/LoginActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.passonatetech.agrohub.R;
+import com.passonatetech.agrohub.cropcalender.CropCalender;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +20,32 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         TextView createnewaccount=findViewById(R.id.dont_have_account);
+        EditText usernameEditText = findViewById(R.id.username);
+        EditText passwordEditText = findViewById(R.id.email);
+        Button submitButton = findViewById(R.id.loginbtn);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (TextUtils.isEmpty(username)) {
+                    usernameEditText.setError("Please enter a valid username.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    passwordEditText.setError("Please enter a valid password.");
+                    return;
+                }else {
+                    loginsuccess();
+                }
+
+                // Perform further validation and authentication here.
+            }
+        });
+
+
         createnewaccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,7 +53,11 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-
+    private void loginsuccess() {
+        Intent intent=new Intent(LoginActivity.this, CropCalender.class);
+        startActivity(intent);
+        finish();
+    }
     private void gotosignup() {
         Intent intent=new Intent(LoginActivity.this,RegistrationActivity.class);
         startActivity(intent);
