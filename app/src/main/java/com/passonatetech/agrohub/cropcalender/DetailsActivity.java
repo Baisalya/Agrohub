e70576a8374d0dcc75eb6922a16b35c4459c40df
Baisalya <baishalya1999@gmail.com>
2023-03-19 17:38:48 +0530

    season and types crops dro down added

--- app/src/main/java/com/passonatetech/agrohub/cropcalender/DetailsActivity.java
+++ app/src/main/java/com/passonatetech/agrohub/cropcalender/DetailsActivity.java
@@ -32,16 +32,19 @@ public class DetailsActivity extends AppCompatActivity {
     private String[] In_Zaid_Legumes;
     private String[] In_Zaid_SeedPlants;
     private String[] In_Zaid_CashCrops;*/
-/**** ****/
-private Spinner seasonspinner,typesofcrops,cropsname;
+    /**** ****/
+
+    private Spinner seasonspinner, typesofcrops, cropsname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        seasonspinner=findViewById(R.id.season_spinner);
-        typesofcrops=findViewById(R.id.types_of_crop_spinner);
-        cropsname=findViewById(R.id.crop_name_spinner);
+
+        Spinner seasonSpinner = findViewById(R.id.season_spinner);
+        Spinner categorySpinner = findViewById(R.id.types_of_crop_spinner);
+        Spinner typeSpinner = findViewById(R.id.crop_name_spinner);
+        // In_Rabi_Cereals = getResources().getStringArray(R.array.In_Rabi_Cereals);
 /*        seasonSpinner = findViewById(R.id.season_spinner);
         seasons = getResources().getStringArray(R.array.seasons);
         cropsinrabi = getResources().getStringArray(R.array.In_Rabi);
@@ -81,42 +84,123 @@ private Spinner seasonspinner,typesofcrops,cropsname;
         });*/
 
         // ArrayAdapter for the season spinner
-        ArrayAdapter<CharSequence> seasonAdapter  = ArrayAdapter.createFromResource(this,
-                R.array.seasons, android.R.layout.simple_spinner_item);
-        seasonAdapter .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        seasonspinner.setAdapter(seasonAdapter );
-        seasonspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        ArrayAdapter<CharSequence> seasonAdapter = ArrayAdapter.createFromResource(
+                this, R.array.seasons, android.R.layout.simple_spinner_item);
+        seasonAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seasonSpinner.setAdapter(seasonAdapter);
+        seasonSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String[] categories = null;
+                switch (position) {
+                    case 0: // Rabi
+                        categories = getResources().getStringArray(R.array.In_Rabi);
+                        break;
+                    case 1: // Kharif
+                        categories = getResources().getStringArray(R.array.In_Kharif);
+                        break;
+                    case 2: // Zaid
+                        categories = getResources().getStringArray(R.array.In_zaid);
+                        break;
+                }
+                ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(
+                        DetailsActivity.this, android.R.layout.simple_spinner_item, categories);
+                categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                categorySpinner.setAdapter(categoryAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
+
+// ArrayAdapter for the crop type spinner
+ /*       typesofcrops.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
                 // Get selected season
-                String season = parent.getItemAtPosition(position).toString();
+                String cropsname = parent.getItemAtPosition(position).toString();
                 // Create array adapter for item spinner based on selected season
                 ArrayAdapter<CharSequence> itemAdapter;
-                if (season.equals("Rabi")) {
-                    itemAdapter = ArrayAdapter.createFromResource(DetailsActivity.this,
-                            R.array.In_Rabi, android.R.layout.simple_spinner_item);
-                    typesofcrops.setVisibility(View.VISIBLE);
-                } else if (season.equals("Kharif")) {
-                    itemAdapter = ArrayAdapter.createFromResource(DetailsActivity.this,
-                            R.array.In_Kharif, android.R.layout.simple_spinner_item);
-                    typesofcrops.setVisibility(View.VISIBLE);
-                } else {
-                    itemAdapter = ArrayAdapter.createFromResource(DetailsActivity.this,
-                            R.array.In_zaid, android.R.layout.simple_spinner_item);
-                    typesofcrops.setVisibility(View.VISIBLE);
-                }
-                itemAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                typesofcrops.setAdapter(itemAdapter);
+                if(cropsname.equals("") ){}
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
+        });*/
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String[] types = null;
+                switch (position) {
+                    case 0: // Cereals
+                        types = getResources().getStringArray(R.array.In_Rabi_Cereals);
+                        break;
+                    case 1: // Vegetables
+                        types = getResources().getStringArray(R.array.In_Rabi_vegetables);
+                        break;
+                    case 2: // Fruits
+                        types = getResources().getStringArray(R.array.In_Rabi_Fruits);
+                        break;
+                    case 3: // Legumes
+                        types = getResources().getStringArray(R.array.In_Rabi_Legumes);
+                        break;
+                    case 4: // Cereals
+                        types = getResources().getStringArray(R.array.In_Kharif_Cereals);
+                        break;
+                    case 5: // vegetables
+                        types = getResources().getStringArray(R.array.In_Kharif_vegetables);
+                        break;
+                    case 6: // Fruits
+                        types = getResources().getStringArray(R.array.In_Kharif_Fruits);
+                        break;
+                    case 7: // Legumes
+                        types = getResources().getStringArray(R.array.In_Kharif_Legumes);
+                        break;
+                    case 8: // SeedPlants
+                        types = getResources().getStringArray(R.array.In_Kharif_SeedPlants);
+                        break;
+                    case 9: // Medicinal
+                        types = getResources().getStringArray(R.array.In_Kharif_Medicinal);
+                        break;
+                    case 10: //Cereals
+                        types = getResources().getStringArray(R.array.In_Zaid_Cereals);
+                        break;
+                    case 11: // vegetables
+                        types = getResources().getStringArray(R.array.In_Zaid_vegetables);
+                        break;
+                    case 12: // Fruits
+                        types = getResources().getStringArray(R.array.In_Zaid_Fruits);
+                        break;
+                    case 13: // Legumes
+                        types = getResources().getStringArray(R.array.In_Zaid_Legumes);
+                        break;
+                    case 14: // SeedPlants
+                        types = getResources().getStringArray(R.array.In_Zaid_SeedPlants);
+                        break;
+                    case 15: // CashCrops
+                        types = getResources().getStringArray(R.array.In_Zaid_CashCrops);
+                        break;
+
+                    default:
+                        types = new String[]{};
+
+                }
+
+                ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, types);
+                categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                typeSpinner.setAdapter(categoryAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //
+            }
         });
-
-// ArrayAdapter for the crop type spinner
-
 
 // ArrayAdapter for the crop name spinner
 
